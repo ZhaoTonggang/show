752c534f8afc1a17707c1760543886716e2847a7
root <root@cvm-3iqxkwe2da225.novalocal>
2022-01-10 16:57:41 +0800

    UI优化

--- sw.js
+++ sw.js
@@ -1,72 +1,16 @@
-const version = 1;
-const cacheStorageKey = "testCache-" + version;
+const CACHE = "pwabuilder-offline";
 
-const cacheList = [
-"./", 
-"./index.html", 
-"./manifest.json", 
-"./css/amazeui.min.css", 
-"./css/bootstrap.min.css", 
-"./css/fonts.css", 
-"./css/main.css", 
-"./icons/512x512.png", 
-"./icons/72x72.png", 
-"./icons/96x96.png", 
-"./icons/128x128.png", 
-"./icons/144x144.png", 
-"./icons/152x152.png", 
-"./icons/192x192.png", 
-"./icons/384x384.png", 
-"./js/bootstrap.min.js", 
-"./js/jquery-3.5.1.min.js", 
-"./js/layer.js", 
-"./js/pwa.js",
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener("install", function(e) {
-//  console.log("Cache event!");
-  e.waitUntil(
-    caches.open(cacheStorageKey).then(function(cache) {
-//      console.log("Adding to Cache:", cacheList);
-      return cache.addAll(cacheList);
-    })
-
-  );
-});
-
-self.addEventListener("activate", event => {
-//  console.log("Activate event");
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(cacheName => cacheStorageKey !== cacheName);
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => {
-//        console.log("Clients claims.");
-        self.clients.claim();
-      })
-  );
-});
-
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
-self.addEventListener("message", event => {
-  if (event.data === "skipWaiting") {
-//    console.log("Skip waiting!");
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
