7f175c2489edb8267d32ed7df91effb5db80d8c7
root <root@cvm-3iqxkwe2da225.novalocal>
2022-01-18 04:37:55 +0800

    初次提交

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 //This is the service worker with the Advanced caching
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('./lib/workbox-sw.js');
 
 const HTML_CACHE = "html";
 const JS_CACHE = "javascript";
